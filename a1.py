d3d50a7ee7950a23dafbf05c696f75d30b489146
Alisha Mishra <alishm1@uci.edu>
2024-01-28 17:59:58 -0800

    import statements

---
+++ a1.py
@@ -0,0 +1,14 @@
+# a1.py
+
+# Starter code for assignment 1 in ICS 32 Programming with Software Libraries in Python
+
+# Replace the following placeholders with your information.
+
+# Alisha Mishra
+# alishm1@uci.edu
+# 33086301
+
+import os
+import sys
+from pathlib import Path
+
